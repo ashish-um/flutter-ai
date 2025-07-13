13861be88f4cee3d987d9129e859a7a291827d6b
Ashish Umrao <ashishumrao632005@gmail.com>
2025-07-13 19:52:34 +0530

    onboarding UI finished

---
+++ lib/model/onboard.dart
@@ -0,0 +1,10 @@
+class Onboard {
+  final String title, subtitle, lottie, buttonText;
+
+  Onboard({
+    required this.title,
+    required this.subtitle,
+    required this.lottie,
+    required this.buttonText,
+  });
+}
--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,3 +1,6 @@
+import 'package:ai_assistant/model/onboard.dart';
+import 'package:ai_assistant/screens/home_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import '../helper/global.dart';
@@ -7,76 +10,115 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final c = PageController();
+
+    final list = [
+      Onboard(
+        title: 'Ask me Anything',
+        subtitle: "I can help you with anything, don't hesitate, just ask",
+        lottie: 'onboard',
+        buttonText: 'Next',
+      ),
+      Onboard(
+        title: "Your Smart AI Assistant",
+        subtitle: "Cutting Edge AI at your fingertips",
+        lottie: 'onboard2',
+        buttonText: "Finish",
+      ),
+    ];
+
     return Scaffold(
-      body: Column(
-        children: [
-          // Spacer(),
-          Lottie.asset('assets/lottie/onboard.json', height: mq.height * 0.55),
-          // Spacer(flex: 5),
+      body: PageView.builder(
+        controller: c,
+        itemCount: list.length,
+        itemBuilder: (ctx, ind) {
+          return Column(
+            children: [
+              // Spacer(),
+              Lottie.asset(
+                'assets/lottie/${list[ind].lottie}.json',
+                height: mq.height * 0.55,
+              ),
+              // Spacer(flex: 5),
 
-          // Title
-          Text(
-            'Ask me Anything',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w900,
-              letterSpacing: 0.5,
-            ),
-          ),
-
-          // Spacing
-          SizedBox(height: mq.height * 0.02),
-
-          // Subtitle
-          SizedBox(
-            width: mq.width * 0.8,
-            child: Text(
-              "I can help you with anything, don't hesitate, just ask",
-              style: TextStyle(fontSize: 13, letterSpacing: 0.5),
-              textAlign: TextAlign.center,
-            ),
-          ),
-
-          Spacer(),
-          // dots
-          Wrap(
-            spacing: 10,
-            children: List.generate(
-              2,
-              (i) => Container(
-                width: 10,
-                height: 8,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadiusGeometry.circular(5),
+              // Title
+              Text(
+                list[ind].title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w900,
+                  letterSpacing: 0.5,
                 ),
               ),
-            ),
-          ),
 
-          Spacer(),
+              // Spacing
+              SizedBox(height: mq.height * 0.02),
 
-          // button
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              shape: StadiumBorder(),
-              elevation: 0,
-              backgroundColor: Colors.blue,
-              minimumSize: Size(mq.width * 0.4, 50),
-            ),
-            child: Text(
-              'Next',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.w800,
+              // Subtitle
+              SizedBox(
+                width: mq.width * 0.8,
+                child: Text(
+                  list[ind].subtitle,
+                  style: TextStyle(fontSize: 13, letterSpacing: 0.5),
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
-          ),
 
-          Spacer(flex: 2),
-        ],
+              Spacer(),
+              // dots
+              Wrap(
+                spacing: 10,
+                children: List.generate(
+                  list.length,
+                  (i) => Container(
+                    width: i == ind ? 15 : 10,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      color: i == ind ? Colors.blue : Colors.grey,
+                      borderRadius: BorderRadiusGeometry.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+
+              Spacer(),
+
+              // button
+              ElevatedButton(
+                onPressed: () {
+                  if (ind != list.length - 1) {
+                    c.nextPage(
+                      duration: Duration(microseconds: 1600),
+                      curve: Curves.ease,
+                    );
+                  } else {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (_) => const HomeScreen()),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  elevation: 0,
+                  backgroundColor: list[ind].lottie != 'onboard'
+                      ? Colors.green.shade600
+                      : Colors.blue,
+                  minimumSize: Size(mq.width * 0.4, 50),
+                ),
+                child: Text(
+                  list[ind].buttonText,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+
+              Spacer(flex: 2),
+            ],
+          );
+        },
       ),
     );
   }

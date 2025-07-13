00ad35d2e6107b0c636ae217d54adfd15594309e
Ashish Umrao <ashishumrao632005@gmail.com>
2025-07-13 19:10:51 +0530

    onboarding page 1 UI

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import '../helper/global.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -9,9 +10,72 @@ class OnboardingScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          // Spacer(),
+          Lottie.asset('assets/lottie/onboard.json', height: mq.height * 0.55),
+          // Spacer(flex: 5),
+
+          // Title
+          Text(
+            'Ask me Anything',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w900,
+              letterSpacing: 0.5,
+            ),
+          ),
+
+          // Spacing
+          SizedBox(height: mq.height * 0.02),
+
+          // Subtitle
+          SizedBox(
+            width: mq.width * 0.8,
+            child: Text(
+              "I can help you with anything, don't hesitate, just ask",
+              style: TextStyle(fontSize: 13, letterSpacing: 0.5),
+              textAlign: TextAlign.center,
+            ),
+          ),
+
           Spacer(),
-          Lottie.asset('assets/lottie/onboard.json'),
-          Spacer(flex: 5),
+          // dots
+          Wrap(
+            spacing: 10,
+            children: List.generate(
+              2,
+              (i) => Container(
+                width: 10,
+                height: 8,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadiusGeometry.circular(5),
+                ),
+              ),
+            ),
+          ),
+
+          Spacer(),
+
+          // button
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: StadiumBorder(),
+              elevation: 0,
+              backgroundColor: Colors.blue,
+              minimumSize: Size(mq.width * 0.4, 50),
+            ),
+            child: Text(
+              'Next',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
+
+          Spacer(flex: 2),
         ],
       ),
     );
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 3), () {
+    Future.delayed(Duration(seconds: 6), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (_) => const OnboardingScreen()),
       );
